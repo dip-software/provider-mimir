3efb4c08a4eb9ff7f2ba977c42ed88590464a2cf
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2025-11-19 16:00:46 +0100

    Refactor resource configurators to improve code readability
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- config/namespaced/alertmanager/config.go
+++ config/namespaced/alertmanager/config.go
@@ -5,8 +5,8 @@ import "github.com/crossplane/upjet/v2/pkg/config"
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("mimir_alertmanager_config", func(r *config.Resource) {
-// We need to override the default group that upjet generated for
-// this resource, which would be "mimir"
-r.ShortGroup = "alertmanager"
-})
+		// We need to override the default group that upjet generated for
+		// this resource, which would be "mimir"
+		r.ShortGroup = "alertmanager"
+	})
 }
